78aafdc780890f6637ef234bc86a33e99e443c2a
Gray Calhoun <gray.calhoun@gmail.com>
2011-12-29 12:31:11 -0600

    Fixed bug in dmw_lambda
    I had ignored the recursive window cases where pi is infinity or zero; this
    caused problems in the mixedwindow function since I set pi to be infinity.

---
+++ oosanalysis/inst/tests/test-dmw_lambda.R
@@ -0,0 +1,8 @@
+test_that("dmw_lambda works as expected", {
+  expect_that(dmw_lambda(Inf, "recursive"), equals(list(fh = 1, hh = 2)))
+  expect_that(dmw_lambda(Inf, "rolling"),   equals(list(fh = 1, hh = 1)))
+  expect_that(dmw_lambda(Inf, "fixed"),     equals(list(fh = 0, hh = Inf)))
+  expect_that(dmw_lambda(0, "recursive"), equals(list(fh = 0, hh = 0)))
+  expect_that(dmw_lambda(0, "rolling"),   equals(list(fh = 0, hh = 0)))
+  expect_that(dmw_lambda(0, "fixed"),     equals(list(fh = 0, hh = 0)))
+})
